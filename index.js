6d0e851a5c6c4b4989e9ffa0da153fee9019aa98
AnonymousPhython <78703146+AnonymousPhython@users.noreply.github.com>
2022-03-30 23:45:30 +0530

    add node modules

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("OK");
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const app = express();
+
+app.use(bodyParser.urlencoded({extended: true}));// --> nessasary code to start parsing through the body of the post request.
+app.use(express.static("public"));  // public is the name of the folder which we will keep as a static folder
