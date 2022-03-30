1ff52e4b234d16ec3f6789c18fd200949e03966b
AnonymousPhython <78703146+AnonymousPhython@users.noreply.github.com>
2022-03-31 00:30:57 +0530

    delopoy in heroku

--- index.js
+++ index.js
@@ -5,3 +5,11 @@ const app = express();
 
 app.use(bodyParser.urlencoded({extended: true}));// --> nessasary code to start parsing through the body of the post request.
 app.use(express.static("public"));  // public is the name of the folder which we will keep as a static folder
+
+app.listen(process.env.PORT || 3000,function(){  // --> app will work both on local system and heroku servers
+  console.log("Server is running on port 3000");
+});
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/index.html");
+});
