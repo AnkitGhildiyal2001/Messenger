c1b351d48478975248dab7f40e7fb5a70e571929
AnonymousPhython <78703146+AnonymousPhython@users.noreply.github.com>
2022-07-11 03:33:31 +0530

    Alexis Pharmacy

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const request = require("request");
 const store = require(__dirname + "/functionality.js");
 const app = express();
+const axios = require('axios');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({
@@ -14,27 +15,45 @@ app.listen(process.env.PORT || 3000, function() { // --> app will work both on l
   console.log("Server is running on port 3000");
 });
 
-var itemsStore = [
-  ["test name", "test description", 32.72],
-  ["test name", "test description", 52.03],
-  ["test name", "test description", 302.56],
-  ["test name", "test description", 78.00]
-];
+// var itemsStore = [
+//   ["test name", "test description", 32.72],
+//   ["test name", "test description", 52.03],
+//   ["test name", "test description", 302.56],
+//   ["test name", "test description", 78.00]
+// ];
+
+// request
 
 var items = [];
 
+axios
+  .post('https://anonympy.service-now.com/api/snc/alexis_pharmacy/update', {
+
+  })
+  .then(res => {
+    console.log(`statusCode: ${res.status}`);
+    items = res.data.result;
+    console.log(items);
+  })
+  .catch(error => {
+    console.error(error);
+  });
+
+
+
+
 var total = 0;
-
-function getItem(name, description, mrp) {
-  this.name = name;
-  this.quantity = 0;
-  this.description = description;
-  this.mrp = mrp;
-}
-
-for (var i = 0; i < itemsStore.length; i++) {
-  items.push(new getItem(itemsStore[i][0], itemsStore[i][1], itemsStore[i][2]));
-}
+//
+// function getItem(name, description, mrp) {
+//   this.name = name;
+//   this.quantity = 0;
+//   this.description = description;
+//   this.mrp = mrp;
+// }
+//
+// for (var i = 0; i < itemsStore.length; i++) {
+//   items.push(new getItem(itemsStore[i][0], itemsStore[i][1], itemsStore[i][2]));
+// }
 
 app.get("/", function(req, res) {
   res.render('index', {
